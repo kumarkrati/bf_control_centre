5ff9365f3c3d3193e554ee01531837da034b0324
アルハム <omegaui22@gmail.com>
2025-09-28 20:23:28 +0530

    🎯changes

--- client/lib/core/server_utils.dart
+++ client/lib/core/server_utils.dart
@@ -9,6 +9,8 @@ enum LoginStatus {
 class ServerUtils {
   ServerUtils._();
 
+  static const _api = 'http://localhost:8001/v1/';
+
   static Future<LoginStatus> login(String username, String password) async {
     // TODO: Implement later
     return LoginStatus.success;
