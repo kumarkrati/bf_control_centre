57821065c55abb3c54ec23266e724932691747ed
アルハム <omegaui22@gmail.com>
2025-09-28 13:10:30 +0530

    🎯update logger

--- server/core/app_logger.ts
+++ server/core/app_logger.ts
@@ -20,43 +20,51 @@ export class AppLogger {
     const date = Date.now();
     const day = formatDate(date);
     const time = formatTime(date);
+    msg = `[${day} (${time})] ${msg}`;
     Deno.writeTextFileSync(
       this.logName,
-      `\n[${day} (${time})] ${msg}`,
+      `\n${msg}`,
       { append: true },
     );
+    console.log(msg)
   }
 
   error(msg: string) {
     const date = Date.now();
     const day = formatDate(date);
     const time = formatTime(date);
+    msg = `(ERROR)[${day} (${time})] ${msg}`;
     Deno.writeTextFileSync(
       this.logName,
-      `\n(ERROR)[${day} (${time})] ${msg}`,
+      `\n${msg}`,
       { append: true },
     );
+    console.error(msg)
   }
 
   warning(msg: string) {
     const date = Date.now();
     const day = formatDate(date);
     const time = formatTime(date);
+    msg = `(WARNING)[${day} (${time})] ${msg}`;
     Deno.writeTextFileSync(
       this.logName,
-      `\n(WARNING)[${day} (${time})] ${msg}`,
+      `\n${msg}`,
       { append: true },
     );
+    console.warn(msg)
   }
 
   fatal(msg: string) {
     const date = Date.now();
     const day = formatDate(date);
     const time = formatTime(date);
+    msg = `(FATAL)[${day} (${time})] ${msg}`;
     Deno.writeTextFileSync(
       this.logName,
-      `\n(FATAL)[${day} (${time})] ${msg}`,
+      `\n${msg}`,
       { append: true },
     );
+    console.error(msg)
   }
 }
