e1eb168e01070ea8db8743ca79f400db9a3b9505
omegaui <omegaui22@gmail.com>
2025-09-29 11:18:23 +0000

    🎯changes

--- server/v1/health.ts
+++ server/v1/health.ts
@@ -2,6 +2,7 @@ import { Hono, Context } from "hono";
 
 export function health(app: Hono) {
   app.get("/v1/health", (context: Context) => {
+    console.log(`sending health ...`);
     return context.json({ message: "Stable v1" });
   });
 }
